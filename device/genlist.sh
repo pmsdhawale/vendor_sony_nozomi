dbd3ebc8febf676f655454c296a9230f541d8a96
RaymanFX <raymanfx@gmail.com>
2013-05-10 10:14:39 +0200

    nozomi: latest qcom blobs
    Change-Id: Ie4a5733065f4372e3c8103d8af8f6e5f9c38caed

---
+++ device/genlist.sh
@@ -0,0 +1,1 @@
+for i in `find . | sed "s:^.::g"`; do echo vendor/sony/lt26/prop-cm10$i:system$i \\ ; done
--- prop-cm10/genlist.sh
+++
@@ -1,1 +0,0 @@
-for i in `find . | sed "s:^.::g"`; do echo vendor/sony/lt26/prop-cm10$i:system$i \\ ; done
