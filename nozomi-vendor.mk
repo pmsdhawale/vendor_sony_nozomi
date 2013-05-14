2461064d6f3801b97f4be6df3219221393737441
RaymanFX <raymanfx@gmail.com>
2013-05-14 15:56:11 +0200

    qcom: update blobs to latest EOS mako
    Change-Id: I07412c3d0ca2642676b5147bc15067cff06fe89a

--- nozomi-vendor.mk
+++ nozomi-vendor.mk
@@ -197,6 +197,7 @@ vendor/sony/nozomi/qcom-common/proprietary/lib/egl/eglsubAndroid.so:system/lib/e
 vendor/sony/nozomi/qcom-common/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+vendor/sony/nozomi/qcom-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
@@ -205,6 +206,7 @@ vendor/sony/nozomi/qcom-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG
 vendor/sony/nozomi/qcom-common/proprietary/lib/libc2d2_a3xx.so:system/lib/libc2d2_a3xx.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+vendor/sony/nozomi/qcom-common/proprietary/lib/libmercury.so:system/lib/libmercury.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/libllvm-a3xx.so:system/lib/libllvm-a3xx.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
 vendor/sony/nozomi/qcom-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
